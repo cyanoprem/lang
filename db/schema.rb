3187494095856b16dcaf630c4a8364b3c3fd2709
Cyano Prem <cyano.prem@gmail.com>
2022-11-08 16:12:50 +0530

    Add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_08_100910) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string "value"
     t.string "language"
